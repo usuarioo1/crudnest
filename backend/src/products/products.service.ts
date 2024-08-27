8fd6cabcbffd48c9d231c54a49f6cfac53853d62
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2024-08-26 23:55:46 -0400

    backs only

---
+++ backend/src/products/products.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+
+constructor(private prismaService: PrismaService){
+
+}
+
+  create(createProductDto: CreateProductDto) {
+    this.prismaService.product.create({data: createProductDto})
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
